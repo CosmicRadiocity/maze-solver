0ad8f4ec55260904edd921b6509a2c3f7d30dbfd
CosmicRadiocity <heartbreakersaturn@gmail.com>
2025-04-07 21:37:53 +0200

    line class

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from window import Window
+from line import Line, Point
 
 def main():
     win = Window(800, 600)
+    start = Point(1, 100)
+    end = Point(10, 100)
+    line = Line(start, end)
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 main()
